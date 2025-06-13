60be9ada0dcfc36a08e4e718347a6b7684cd97a2
Nicolas5071-14 <ramahalefitra.abelson.nicolas@gmail.com>
2025-06-13 10:10:30 +0300

    React page1

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,20 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+     jsxRuntime: 'classic'
+  })],
   root: './',
   base: '/portfolio/',
+  esbuild: {
+    loader: 'jsx', 
+  },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: './index.html' 
+        main: './index.html'
       }
     }
   }
